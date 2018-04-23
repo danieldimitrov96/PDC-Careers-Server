b6acfdc2b7998f369c221e5b875549035ecd698f
Daniel Dimitrov <dakata962@gmail.com>
2018-04-23 11:05:34 +0300

    Update seeds

--- app/db/seed/contacts.seed.js
+++ app/db/seed/contacts.seed.js
@@ -1,23 +1,38 @@
 const seeder = require('mongoose-seed');
-const connectionString = require('../config');
+const connectionString = require('./../../config').URL;
 
 seeder.connect(connectionString, function () {
     seeder.loadModels([
         'app/db//models/contact/Contact.js',
     ]);
 
-    // seeder.clearModels(['Contact'], function () {
+    seeder.clearModels(['Contact'], function () {
         seeder.populateModels(data, function () {
             seeder.disconnect();
         });
     });
-// });
+});
 
 const data = [{
     'model': 'Contact',
     'documents': [{
         'name': 'gosho EOOD',
+        'address': 'pirotska SOFIA',
+        'isPrimary': false,
+    },
+    {
+        'name': 'tosho EOOD',
         'address': 'SOFIA',
+        'isPrimary': false,
+    },
+    {
+        'name': 'pesho EOOD',
+        'address': 'sevastopol varna',
+        'isPrimary': true,
+    },
+    {
+        'name': 'ivan EOOD',
+        'address': 'ndk SOFIA',
         'isPrimary': true,
     },
     ],
